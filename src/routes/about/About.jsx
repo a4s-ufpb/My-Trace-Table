e75f7690d01cfe696e449675d60fdbd65d133636
Elksandro2 <silvaelksandro@gmail.com>
2025-08-01 11:05:02 -0300

    Chore: atualizando texto do Sobre, removendo fotos e adicionando LinkedIn

--- src/routes/about/About.jsx
+++ src/routes/about/About.jsx
@@ -7,41 +7,11 @@ function About() {
       <SecondaryHeader showBackButton={true} title="Sobre" />
       <div className="center-content">
         <div className="wrapper">
-          <div className="about-section">
-            <img src="/diego.webp" alt="Diego Tavares" className="about-image" />
-            <p>
-              Esta aplicação surgiu como parte do projeto de <a href="https://drive.google.com/file/d/1Ka-vqvY3T3M0S7I3xjbJQK_2u7MB_d4W/view" target="_blank">TCC</a> do aluno <span>Diego Tavares</span>. Inspirado pela
-              necessidade de uma ferramenta interativa e educacional, Diego concebeu a ideia do projeto e foi responsável
-              por criar a interface inicial, imaginando uma plataforma que facilitasse o aprendizado e aprimorasse a prática dos usuários.
-            </p>
-          </div>
+          <p className="description">O <span>My Trace Table Manager</span> é uma aplicação desenvolvida como parte do projeto <span>Apps4Society</span>, da UFPB Campus IV, que tem como objetivo construir aplicativos e sistemas que impactam positivamente a sociedade. Esta aplicação surgiu como parte do projeto de TCC do aluno <a href="https://drive.google.com/file/d/1Ka-vqvY3T3M0S7I3xjbJQK_2u7MB_d4W/view">Diego Tavares</a>. Inspirado pela
+            necessidade de uma ferramenta interativa e educacional, Diego concebeu a ideia do projeto e foi responsável
+            por criar a interface inicial, imaginando uma plataforma que facilitasse o aprendizado e aprimorasse a prática dos usuários.</p>
 
-          <div className="about-section">
-            <img src="/ayla.jpg" alt="Ayla Dantas" className="about-image" />
-            <p>
-              O projeto foi orientado pela professora <span>Ayla Dantas</span>, que guiou o desenvolvimento da ideia e coordenou
-              os aspectos pedagógicos e estruturais do TCC, contribuindo para a concretização do projeto.
-            </p>
-          </div>
-
-          <div className="about-section">
-            <img src="/ronyelison.jpg" alt="Ronyelison Abreu" className="about-image" />
-            <p>
-              O desenvolvimento da aplicação foi realizado por <span>Ronyelison Abreu</span>, que implementou e aperfeiçoou as funcionalidades,
-              transformando a ideia inicial em uma ferramenta prática e acessível para os usuários.
-            </p>
-          </div>
-
-          <div className="about-section">
-            <div className="container-img">
-              <img src="/anna.jpg" alt="Anna Gabriela" className="about-image" />
-              <img src="/elksandro.jpg" alt="José Elksandro" className="about-image" />
-            </div>
-            <p>
-              A continuidade do trabalho foi conduzida por <span>Anna Gabriela</span> e <span>José Elksandro</span>, que deram sequência ao desenvolvimento da plataforma,
-              implementando melhorias e acrescentando novas funcionalidades.
-            </p>
-          </div>
+          <p className="description">A ideia de testes de mesa digitais vem também de outros trabalhos de TCC orientados pela professora Ayla Dantas Rebouças, como o de <a href="https://drive.google.com/file/d/1jctcgeqFaJXe99s-YR-gyrm4B-C9Fsi9/view" target="_blank">Josinaldo Silva</a> e <a href="https://drive.google.com/file/d/1Ez-aqusmi8qApLEDX7I4CKMjVeZx4Z3f/view" target="_blank">Pedro Gomes</a>, inspirados em atividades realizadas pela professora Vanessa Dantas. No projeto Apps4Society, na vigência 2024-2025, o desenvolvimento deste sitema Web foi conduzido incialmente pelo estudante Ronyelison Abreu (<a href="https://www.linkedin.com/in/ronyelison-de-oliveira-abreu" target="_blank">linkedin</a>) e em seguida pelos estudantes Anna Gabriela (<a href="https://www.linkedin.com/in/anna-gabriela-ms" target="_blank">linkedin</a>) e José Elksandro (<a href="https://www.linkedin.com/in/elksandro" target="_blank">linkedin</a>), do curso de Sistemas de Informação. O My Trace Table foi construido sob orientação da professora Ayla Dantas Rebouças, coordenadora do projeto.</p>
         </div>
       </div>
     </div>
