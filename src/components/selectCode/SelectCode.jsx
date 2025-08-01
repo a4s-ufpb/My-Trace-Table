d8183b5a776e5f1a5fc38a392af445783c152fbd
Elksandro2 <silvaelksandro@gmail.com>
2025-08-01 11:36:38 -0300

    Chore: atualizando texto

--- src/components/selectCode/SelectCode.jsx
+++ src/components/selectCode/SelectCode.jsx
@@ -49,7 +49,7 @@ function SelectCode({ setSelectCode }) {
                     onClick={handleButtonClick}
                     type="submit"
                 >
-                    Inserir
+                    Buscar
                 </button>
             </form>
 
